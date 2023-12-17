56b845763abfa3a5ee50ebb88ddc62e358ae517c
sorambr <caecronos@hotmail.com>
2023-12-17 13:26:13 -0300

    (feat/build): Novo visual do progressbar

--- web/src/features/progress/Progressbar.tsx
+++ web/src/features/progress/Progressbar.tsx
@@ -7,11 +7,14 @@ import type { ProgressbarProps } from '../../typings';
 
 const useStyles = createStyles((theme) => ({
   container: {
-    width: 350,
-    height: 45,
+    width: 400,
+    height: 10,
     borderRadius: theme.radius.sm,
-    backgroundColor: theme.colors.dark[5],
+    // backgroundColor: theme.colors.dark[5],
     overflow: 'hidden',
+    backgroundColor: 'rgba(0, 0, 0, 0.685)',
+    clipPath: 'polygon(0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%, 30% 2%, 10% 1%, 30% 5%, 34% 1%, 39% 4%, 43% 5%, 50% 6%, 50% 7%, 53% 3%, 58% 6%, 61% 9%, 68% 4%, 74% 7%, 78% 9%, 84% 5%, 105% 9%, 95% 16%, 98% 19%, 99% 27%, 99% 3%, 96% 45%, 96% 50%, 97% 57%, 97% 61%, 96% 69%, 99% 82%, 95% 88%, 94% 93%, 97% 98%, 82% 99%, 81% 94%, 74% 91%, 61% 96%, 54% 97%, 50% 93%, 35% 99%, 34% 90%, 31% 90%, 15% 94%, 14% 93%, 9% 88%, 1% 90%, 6% 90%, 0% 57%, 2% 45%, 3% 43%, 2% 10%, 9% 20%, 2% 15%, 3% 13%)',
+    textAlign: 'left',
   },
   wrapper: {
     width: '100%',
@@ -24,7 +27,9 @@ const useStyles = createStyles((theme) => ({
   },
   bar: {
     height: '100%',
-    backgroundColor: theme.colors[theme.primaryColor][theme.fn.primaryShade()],
+    backgroundColor: 'rgba(255, 255,255, 0.865)',
+
+    // backgroundColor: theme.colors[theme.primaryColor][theme.fn.primaryShade()],
   },
   labelWrapper: {
     position: 'absolute',
